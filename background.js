7da93b0d3603439497e79e47c54fe09eb979ea78
jbarcossc <juancruzbarcos@hotmail.com>
2024-02-06 23:37:05 -0300

    End logic

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
+const apiUrl = 'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions';
+
 chrome.runtime.onMessage.addListener((msg,sender,response) => {
     if(msg.name === 'fetchGames'){
-        const apiUrl = 'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions';
-        console.log(apiUrl);
-
         fetch(apiUrl).then((res) => {
             if(res.status !== 200) {
                 response({message: "Error"});
@@ -17,4 +16,77 @@ chrome.runtime.onMessage.addListener((msg,sender,response) => {
     }
 
     return true
-})
+});
+
+function checkTimeDifference(data){
+    // Check date
+    const date = new Date().toString();
+    const oldDate = data.lastCheckTime;
+    if (!oldDate) {
+        // If currentTime doesn't exist in storage, save it
+        chrome.storage.local.set({ lastCheckTime: date });
+        oldDate = date;
+    }
+    const diff = Date.parse(date) - Date.parse(oldDate);
+    const timeDiff = diff/1000/60;
+    const res = timeDiff > 0.1;
+    if(res) {
+        chrome.storage.local.set({ lastCheckTime: date });
+    }
+    return res;
+}
+
+function filterGames(games){
+    return games.filter((game) => {
+        return game.price.totalPrice.discountPrice == 0;
+    });
+}
+
+function formatTitle(title, max){
+    return title.length < max ? title : title.slice(0,max+3)+'...';
+}
+
+function updateLocalStorage(newGameData, storageGameData){
+    if(!storageGameData){
+        chrome.storage.local.set({ games: newGameData });
+        return true
+    }
+    storageGameData = filterGames(storageGameData);
+    newGameData = filterGames(newGameData);
+    let res = false;
+    // Check for elements in newGameData that are not in storageGameData and add them to storageGameData
+    newGameData.forEach(item => {
+        if (!storageGameData.some(elem => elem.title === item.title)) {
+            storageGameData.push(item);
+            res = true;
+        }
+    });
+    // Check for elements in storageGameData that are not in newGameData and remove them from storageGameData
+    storageGameData.forEach((item, index) => {
+        if (!newGameData.some(elem => elem.title === item.title)) {
+            storageGameData.splice(index, 1);
+        }
+    });
+    console.log(res, newGameData, storageGameData);
+    return res;
+}
+
+chrome.tabs.onCreated.addListener((tab) => {
+    chrome.storage.local.get('lastCheckTime', (data) => {
+        const timePassed = checkTimeDifference(data);
+        if(!timePassed) return;
+        // Update storage
+        fetch(apiUrl).then((res) => { 
+            res.json().then((data) => {
+                chrome.storage.local.get('games', (storageData) => {
+                    const newGameData = data.data.Catalog.searchStore.elements;
+                    const areNewGames = updateLocalStorage(newGameData, storageData.games);
+                    if(areNewGames){
+                        chrome.action.setIcon({ path: "icon128.png" });
+                    }
+                });
+            });
+        });
+    });
+});
+
--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ function updateHTML(games){
     gamesHTML.innerHTML = "";
     games.forEach((game) => {
         gamesHTML.innerHTML += `
-            <div class="game">
+            <a class="game" href="https://store.epicgames.com/en-US/p/${game.catalogNs.mappings[0].pageSlug}" target="_blank">
                 <div class="game__image-container">
                     <img src="${game.keyImages.find(image => image.type === "OfferImageWide").url}" alt="${game.title} image" class="game__image">
                 </div>
@@ -24,28 +24,23 @@ function updateHTML(games){
                             <p class="game__info-price game__info-price_original">USD$${game.price.totalPrice.originalPrice / 100}</p>
                             <p class="game__info-price game__info-price_discount">USD$${game.price.totalPrice.discountPrice / 100}</p>
                         </div>
-                        <a href="https://store.epicgames.com/en-US/p/${game.catalogNs.mappings[0].pageSlug}" target="_blank" class="game__info-link">
-                            <svg class="game__info-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
-                        </a>
+                        <svg class="game__info-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
                     </div>
                 </div>
-            </div>
+            </a>
             <div class="game-divider"></div>
         `
     });
 }
 
-chrome.runtime.sendMessage({name: 'fetchGames'}, (response) => {
-    // Filter games
-    const games = response.data.data.Catalog.searchStore.elements;
-    const availableGames = filterGames(games);
+function main(){
+    chrome.runtime.sendMessage({name: 'fetchGames'}, (response) => {
+        // Filter games
+        const games = response.data.data.Catalog.searchStore.elements;
+        const availableGames = filterGames(games);
+        // Update HTML
+        updateHTML(games);
+    })
+}
 
-    // Update HTML
-    updateHTML(availableGames);
-
-    // Update local storage
-
-    // Manage popup
-
-    console.log(availableGames);
-})
+main();
