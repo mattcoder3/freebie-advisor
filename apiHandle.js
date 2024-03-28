349c2094b4dd3a403911726b2c9a7dbedef34aa4
jbarcossc <juancruzbarcos@hotmail.com>
2024-03-28 16:56:23 -0300

    Add fetch error handling

--- apiHandle.js
+++ apiHandle.js
@@ -1,10 +1,15 @@
 // Epic Games Store
 async function getEpicGamesGames() {
-	const apiUrl =
-		'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions';
-	const res = await fetch(apiUrl);
-	const data = await res.json();
-	return filterEpicGamesGames(data);
+	try {
+		const apiUrl =
+			'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions';
+		const res = await fetch(apiUrl);
+		const data = await res.json();
+		return filterEpicGamesGames(data);
+	} catch (error) {
+		console.log('Error fetching Epic Games Store Games:', error);
+		return [];
+	}
 }
 
 const filterEpicGamesGames = (data) => {
@@ -26,10 +31,15 @@ const filterEpicGamesGames = (data) => {
 
 // GOG.com
 async function getGOGGames() {
-	const url = 'https://catalog.gog.com/v1/catalog';
-	const response = await fetch(url);
-	const data = await response.json();
-	return filterGOGGames(data);
+	try {
+		const url = 'https://catalog.gog.com/v1/catalog';
+		const response = await fetch(url);
+		const data = await response.json();
+		return filterGOGGames(data);
+	} catch (error) {
+		console.log('Error fetching GOG.com Games:', error);
+		return [];
+	}
 }
 
 const filterGOGGames = (data) => {
@@ -49,11 +59,16 @@ const filterGOGGames = (data) => {
 
 // Humble Bundle
 async function getHumbleBundleGames() {
-	const url =
-		'https://www.humblebundle.com/store/api/search?sort=discount&filter=onsale&request=1';
-	const response = await fetch(url);
-	const data = await response.json();
-	return filterHumbleBundleGames(data);
+	try {
+		const url =
+			'https://www.humblebundle.com/store/api/search?sort=discount&filter=onsale&request=1';
+		const response = await fetch(url);
+		const data = await response.json();
+		return filterHumbleBundleGames(data);
+	} catch (error) {
+		console.log('Error fetching Humble Bundle Games:', error);
+		return [];
+	}
 }
 
 const filterHumbleBundleGames = (data) => {
